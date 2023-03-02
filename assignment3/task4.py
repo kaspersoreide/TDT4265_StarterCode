ca1c5b2b3fb13902e05df901903f6ab39bcf6f81
kaspersoreide <kasper.soreide@gmail.com>
2023-03-02 17:03:04 +0100

    asdasd

--- assignment3/task4.py
+++ assignment3/task4.py
@@ -17,16 +17,17 @@ class MyResnet(nn.Module):
             torchvision.transforms.Resize([224, 224]),
         ])
         self.model = torchvision.models.resnet18(pretrained=True)
-        self.fc = nn.Linear(512, 10) # No need to apply softmax,
+        self.model.fc = nn.Linear(512, 10) # No need to apply softmax,
         # as this is done in nn.CrossEntropyLoss
         for param in self.model.parameters(): # Freeze all parameters
             param.requires_grad = False
+        for param in self.model.fc.parameters(): # Unfreeze the last fully-connected
+            param.requires_grad = True # layer
         for param in self.model.layer4.parameters(): # Unfreeze the last 5 convolutional
             param.requires_grad = True # layers
     def forward(self, x):
         x = self.transform(x)        
         x = self.model(x)
-        x = self.fc(x)
         return x
 
 def create_plots(trainer: Trainer, name: str):
